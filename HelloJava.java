012dc474afb4a6f9094450712f190f46ae71f2c6
zzhh <zzhh20112011@163.com>
2019-04-23 18:08:23 +0800

    bug fixed

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,7 @@ public class HelloJava{
 	public static void main(String[]args)
 	{
 		System.out .println("hello,java");
-		System.out .println("hello,java");
+		System.out .println("hello,java,github");
 		
 		System.out .println("hello,world");
 		System.out .println("hello,world");
