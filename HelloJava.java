922284b8c7cf3a61fab50e7e1a23d28e0f9d7e62
zzhh <zzhh20112011@163.com>
2019-04-23 16:26:20 +0800

    update 4

--- HelloJava.java
+++ HelloJava.java
@@ -2,6 +2,6 @@ public class HelloJava{
 	public static void main(String[]args)
 	{
 		System.out .println("hello,java");
-		System.out .println("hello,java");
+		System.out .println("hello,world");
 	}
 }
